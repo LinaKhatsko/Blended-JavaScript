c6adba34d90eccc08227b267266db49733a26e9f
Lina Khatsko <lina.khatsko@gmail.com>
2025-06-16 23:25:05 +0300

    add les 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,81 +1,45 @@
-const sortByDescendingFriendCount = users =>
- 
-  users.toSorted((a, b) => b.friends.length - a.friends.length);
+// const numbers = [1, 2, 3, 4];
+// numbers.method(number)=> { return number }
+// numbers.method(number => number);
 
-console.log(
-  sortByDescendingFriendCount([
-    {
-      name: "Moore Hensley",
-      friends: ["Sharron Pace"],
-      gender: "male"
-    },
-    {
-      name: "Sharlene Bush",
-      friends: ["Briana Decker", "Sharron Pace"],
-      gender: "female"
-    },
-    {
-      name: "Ross Vazquez",
-      friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-      gender: "male"
-    },
-    {
-      name: "Elma Head",
-      friends: ["Goldie Gentry", "Aisha Tran"],
-      gender: "female"
-    },
-    {
-      name: "Carey Barr",
-      friends: ["Jordan Sampson", "Eddie Strong"],
-      gender: "male"
-    },
-    {
-      name: "Blackburn Dotson",
-      friends: ["Jacklyn Lucas", "Linda Chapman"],
-      gender: "male"
-    },
-    {
-      name: "Sheree Anthony",
-      friends: ["Goldie Gentry", "Briana Decker"],
-      gender: "female"
-    }
-  ])
-);
-// [
-//   {
-//     name: "Ross Vazquez",
-//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-//     gender: "male"
-//   },
-//   {
-//     name: "Sharlene Bush",
-//     friends: ["Briana Decker", "Sharron Pace"],
-//     gender: "female"
-//   },
-//   {
-//     name: "Elma Head",
-//     friends: ["Goldie Gentry", "Aisha Tran"],
-//     gender: "female"
-//   },
-//   {
-//     name: "Carey Barr",
-//     friends: ["Jordan Sampson", "Eddie Strong"],
-//     gender: "male"
-//   },
-//   {
-//     name: "Blackburn Dotson",
-//     friends: ["Jacklyn Lucas", "Linda Chapman"],
-//     gender: "male"
-//   },
-//   {
-//     name: "Sheree Anthony",
-//     friends: ["Goldie Gentry", "Briana Decker"],
-//     gender: "female"
-//   },
-//   {
-//     name: "Moore Hensley",
-//     friends: ["Sharron Pace"],
-//     gender: "male"
-//   }
-// ]
+// const dobleNumbers =[];
+// numbers.forEach(number => dobleNumbers.push(number ** 2));
+// console.log(dobleNumbers);
 
+// const dobleNumbers = numbers.map((number) => { return number ** 2; });
+// const dobleNumbers = numbers.map((number) => number ** 2);
+// console.log(dobleNumbers);
+
+// const getDobleNumbers = (array) => {
+  // return array.map((number) => {
+    // return number ** 2;
+  // });
+// }
+
+// const getDobleNumbers = (array) => array.map((number) => number ** 2;);
+// console.log(dobleNumbers);
+// console.log([2, 4, 8, 16]);
+
+// const data = [
+//   { id: 1, values: [1, 2, 3] },
+//   { id: 2, values: [4, 5, 6] },
+//   { id: 3, values: [7, 8, 9] },
+//  ];
+
+// // const dataValue = data.flatMap(item => item.values);
+// // console.log(dataValue);
+
+// const dataValue = data => data.flatMap(item => item.values);
+// console.log(dataValue(data));
+
+
+const people = [ 
+  { name: 'John', age: 27 }, 
+  { name: 'Jane', age: 31 }, 
+  { name: 'Bob', age: 19 }, 
+];
+
+const hasUnder20 = people.some(person => person.age < 20);
+console.log(hasUnder20);
+
+const getSomePeople = array => 
